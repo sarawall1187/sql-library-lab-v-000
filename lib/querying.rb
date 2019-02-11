77e5347fb63300d34df38f1c439f14f6e6e64ae0
Sara Walker <sarawall1187@gmail.com>
2019-02-11 20:27:46 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,5 +20,5 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, COUNT(character_books) JOIN characters ON character_books.character_id = characters.id DESC;"
+  "SELECT characters.name, SUM(character_books) JOIN characters ON character_books.character_id = characters.id DESC;"
 end
